e50d8a0516fcb82f80efc2db2bbfeaaaaf6916b8
ashleydonohoe <ashleyrdonohoe@gmail.com>
2017-07-05 00:23:27 -0400

    Added Saved Songs activity

---
+++ app/src/main/java/com/adonohoe/justplaymusic/SavedSongsActivity.java
@@ -0,0 +1,13 @@
+package com.adonohoe.justplaymusic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SavedSongsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_songs);
+    }
+}
